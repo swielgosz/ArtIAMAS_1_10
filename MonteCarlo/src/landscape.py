0087436c9984446ab1cedf579eab4e249d1d15d1
swielgosz <swielgosz20@gmail.com>
2024-02-14 19:14:20 -0500

    Added comments to entire code. Terms in quotes in landscape.py refer to specific terminology I defined in ARL ppt

--- MonteCarlo/src/landscape.py
+++ MonteCarlo/src/landscape.py
@@ -1,21 +1,25 @@
-
-
 import matplotlib.pyplot as plt
 
 import numpy as np
 import pprint as pp
 
 
-SENSOR_RADIUS = 2
+SENSOR_RADIUS = 2 # max sensor radius
 K = 10000
 
+# For landscape classes:
+# Color: used for plotting
+# Land_type: type of terrain
+# is_sensor_valid: describes if a sensor can be placed on that terrain
+
+# Water class
 class Water:
     def __init__(self):
         self.color = (10, 62, 235)
         self.land_type = 'water'
-        self.is_sensor_valid = False
+        self.is_sensor_valid = False 
 
-
+# Land class
 class Land: 
     def __init__(self):
         # self.color = (250, 245, 145)
@@ -24,13 +28,14 @@ class Land:
         self.land_type = 'land'
         self.is_sensor_valid = True
 
+# Road class
 class Road:
     def __init__(self):
         self.color = (0, 0, 0)
         self.land_type = 'road'
         self.is_sensor_valid = False
 
-
+# Node class
 class Node:
     def __init__(self, x, y, land_type=None, veg_level=0):
 
@@ -44,9 +49,9 @@ class Node:
         self.sensor_radius = 0
 
 
-
-
 class Configuration:
+
+    # Constructor
     def __init__(self, width, height):
 
         self.width = width
@@ -60,20 +65,23 @@ class Configuration:
                 row.append(Node(i, j, Land()))
             self.grid.append(row)
 
+    # Return terrain type of each grid point (as a word rather than numeric value)
     def print_grid(self):
         for row in self.grid:
             for node in row:
                 print(node.land_type, end=' ')
             print()
 
+    # Load terrain file from csv
     def load_from_csv(self, filepath):
         
+        # Read in numberic data (where each number in the csv file correlates to a terrain type)
         with open(filepath, 'r') as f:
             lines = f.readlines()
             for i in range(len(lines)):
                 lines[i] = lines[i].strip().split(',')
                     
-
+        # Define terrain type (and veg_level for land) according to numeric value at each grid point
         for i in range(len(lines)):
             for j in range(len(lines[i])):
                 item = lines[i][j].strip()
@@ -90,6 +98,7 @@ class Configuration:
                         veg_level = 1
                     self.grid[i][j].veg_level = veg_level
 
+    # Check if a sensor can be placed at a specific grid point
     def is_valid_sensor_location(self, i, j):
 
         if self.grid[i][j].land_type.is_sensor_valid:
@@ -97,24 +106,29 @@ class Configuration:
         
         return False
     
-
+    # Populate grid point with sensor
     def deploy_sensor(self, i, j, sensor_type, sensor_radius):
         self.grid[i][j].sensor_type = sensor_type # "acoustic" or "seismic"
         self.grid[i][j].sensor_radius = sensor_radius
 
+    # Clear sensors from grid points
     def clear_sensors(self):
         for i in range(self.height):
             for j in range(self.width):
                 self.grid[i][j].sensor_type = None
 
+    # PLotting the terrain with sensors
     def plot_grid(self, s_map=None):
         
         data = self.grid
 
+        # First plot terrain with unique colors for each terrain type
         for i in range(self.height):
             for j in range(self.width):
 
                 if self.grid[i][j].land_type.land_type == "land":
+                    # Distinguish between low and high vegetation
+                    # TASK change this, this was a last minute addition and not well done
                     if self.grid[i][j].veg_level < 0.5:
                         data[i][j] = self.grid[i][j].land_type.color
                     else:
@@ -122,13 +136,14 @@ class Configuration:
                 elif self.grid[i][j].land_type != None:
                     data[i][j] = self.grid[i][j].land_type.color
                 else:
-                    data[i][j] = (255, 255, 255)
+                    data[i][j] = (255, 255, 255) # black if there is no terrain
 
         fig, ax = plt.subplots()
 
         plt.title(f"Best Configuration ")
         ax.imshow(data)
         
+        # Plot sensors with unique color for each modality
         if s_map != None:
             for item in s_map:
                 if item["sensor_type"] == "seismic":
@@ -137,12 +152,15 @@ class Configuration:
                     # sensor_color = '#6e5502' #'#e88e10' # orange
                     sensor_color = '#e88e10' # orange
 
+                # Plotting sensors location
                 circle = plt.Circle((item["j"], item["i"]), item["sensor_radius"], color = sensor_color, linewidth = 2, fill=False)
                 ax.add_patch(circle)
 
+                # Plotting sensor sensing radius
                 circle2 = plt.Circle((item["j"], item["i"]), .5, color = sensor_color,  fill=True)
                 ax.add_patch(circle2)
 
+                # Plotting sensor communication radius
                 circle3 = plt.Circle((item["j"], item["i"]), item["sensor_comm_radius"], color = sensor_color,linestyle='--', linewidth = 0.5,fill=False)
                 ax.add_patch(circle3)
 
@@ -151,6 +169,7 @@ class Configuration:
         plt.show()
         
 
+    # Save plots. This is basically the same as plot_grid. Not efficient
     def save_history(self, history,dirpath):
         c = 0
         for s_map in history:
@@ -198,6 +217,7 @@ class Configuration:
             c += 1
 
 
+    # Assign acoustic sensor component of "coverage score" (defined in project PPT)
     def get_node_acoustic_score(self, i, j):
         
         n = self.grid[i][j]
@@ -206,12 +226,13 @@ class Configuration:
         if n.land_type.land_type == "water":
             scale_factor = 0.5
         elif n.land_type.land_type == "land":
-            scale_factor = 1 / (n.veg_level + 1)
+            scale_factor = 1 / (n.veg_level + 1) # accounts for low vs high vegetation
         elif n.land_type.land_type == "road":
             scale_factor = 1
 
         return a_0 * scale_factor
 
+    # Assign seismic sensor component of "coverage score" (defined in project PP
     def get_node_seismic_score(self, i, j):
         
         n = self.grid[i][j]
@@ -226,6 +247,7 @@ class Configuration:
 
         return a_0 * scale_factor
     
+    # Weighting factor of each terrain type (alpha in PPT)
     def get_desirability_score(self, i, j):
         
         n = self.grid[i][j]
@@ -236,7 +258,38 @@ class Configuration:
             return 1
         elif n.land_type.land_type == "road":
             return 2
+    
+    # Calculate cost (configuration profit in PPT) of configuration. We want to maximize the cost, i.e. maximize information known about the environment
+    def get_configaration_observability(self, s_map=None):
+        if s_map == None:
+            s_map = self.get_sensor_map()
         
+        conf_score = 0
+
+        # Loop through each grid point
+        for i in range(self.height):
+            for j in range(self.width):
+                
+                n_acoustic = 0
+                n_seismic = 0
+
+                # Check which sensors each grid point is detected by, and assign "coverage score" accordingly
+                for sensor in s_map:
+
+                    if np.sqrt((i - sensor["i"])**2 + (j - sensor["j"])**2) <= sensor["sensor_radius"]:
+
+                        if sensor["sensor_type"] == "acoustic" and n_acoustic == 0:
+                            n_acoustic = self.get_node_acoustic_score(i, j)
+
+                        elif sensor["sensor_type"] == "seismic" and n_seismic == 0:
+                            n_seismic = self.get_node_seismic_score(i, j)
+
+                # Calculate "profit" at each grid point by weighting "coverage score" by terrain weighting factor, and sum to find total "configuration profit"
+                conf_score += (n_acoustic + n_seismic) * self.get_desirability_score(i, j)
+
+        return conf_score
+    
+    # Return informaton about sensors in map
     def get_sensor_map(self):
 
         sensor_map = []
@@ -257,13 +310,14 @@ class Configuration:
         
         return sensor_map
 
-
+    # Check if the sensors in a configuration are connected via a WSN
     def is_configuration_valid(self, s_map=None):
         if s_map == None:
             s_map = self.get_sensor_map()
 
         graph = {}
 
+        # For each sensor, record which sensors are within communication radius
         for i in range(len(s_map)):
             curr_node_connections = []
             for j in range(len(s_map)):
@@ -275,7 +329,7 @@ class Configuration:
             graph[str(i)] = curr_node_connections
                 
 
-
+        # Implement breadth first search to check if sensors are connected
         visited = [] # List for visited nodes.
         queue = []     #Initialize a queue
         visited.append('0')
@@ -289,37 +343,9 @@ class Configuration:
                     visited.append(neighbour)
                     queue.append(neighbour)
         
+        # If list of visited neighbors is equal in length to number of sensors, sensors are connected
         if len(visited) == len(s_map):
             return True
-
-
-    def get_configaration_observability(self, s_map=None):
-        if s_map == None:
-            s_map = self.get_sensor_map()
-        
-        conf_score = 0
-        for i in range(self.height):
-            for j in range(self.width):
-                
-                n_acoustic = 0
-                n_seismic = 0
-
-                for sensor in s_map:
-
-                    # if its in range
-                    if np.sqrt((i - sensor["i"])**2 + (j - sensor["j"])**2) <= sensor["sensor_radius"]:
-
-                        if sensor["sensor_type"] == "acoustic" and n_acoustic == 0:
-                            n_acoustic = self.get_node_acoustic_score(i, j)
-
-                        elif sensor["sensor_type"] == "seismic" and n_seismic == 0:
-                            n_seismic = self.get_node_seismic_score(i, j)
-
-                conf_score += (n_acoustic + n_seismic) * self.get_desirability_score(i, j)
-
-        return conf_score
-
-
 
 if __name__ == '__main__':
     landscape = Configuration(100, 100)
--- MonteCarlo/src/main.py
+++ MonteCarlo/src/main.py
@@ -1,5 +1,3 @@
-
-
 from landscape import Configuration, Node
 from sensors import Sensor
 import numpy as np
@@ -7,19 +5,27 @@ import pprint as pp
 import os
 import shutil
 from pathlib import Path
-import csv
-import os.path
 
 
-MAX_ITERS = 100000
-sensor_comm_ratio = 1.5
-CASE = 4
+MAX_ITERS = 100000 # max number of iterations for Monte Carlo simulation
+sensor_comm_ratio = 1.5 # ratio of sensor communication to sensing radius (because communication radius is typically larger)
+CASE = 4 # case number describing combination of sensor radii and modalities:
+         # !! Change later to make more general
+         # Four cases for sensor radius and modality combinations:
+         # Case 1: Uniform sensor modality, sensor sensing radius, and sensor communication radius
+         # Case 2: Two sensor modalities (acoustic and seismic) which are uniformly distributed, and uniform sensor communication radius
+         # Case 3: Uniform sensor modality, random (but bounded) sensor communication radius 
+         # Case 4: Mixed sensor modalities, random sensor communication radius
 
+# Function places sensors in unique random valid locations (i.e. not in water etc.) on map equal in size to provided terrain map
 def make_basic_seismic_map(num_sensors, sensor_radius, max_width, max_height, terrain):
     sensor_map = []
     already_placed = []
 
     for k in range(num_sensors):
+
+        # Place sensors in random locations that are not already occupied by another sensor
+        # Terrain at location (i,j) must be suitable to be occupied by sensor (as noted by attribute is_valid_sensor_location)
         item = {}
 
         j = np.random.randint(0, max_width)
@@ -32,7 +38,8 @@ def make_basic_seismic_map(num_sensors, sensor_radius, max_width, max_height, te
         item["j"] = j
         item["i"] = i
 
-       
+        # !! Change later to make more general
+        # Assign sensor radius and modality according to case 
         if CASE == 1:
             item["sensor_type"] = "seismic"
             item["sensor_radius"] = sensor_radius
@@ -56,26 +63,29 @@ def make_basic_seismic_map(num_sensors, sensor_radius, max_width, max_height, te
             item["sensor_radius"] = sensor_radius[k]
             item["sensor_comm_radius"] = sensor_comm_ratio*sensor_radius[k]
 
-        
+        # Record placed sensors
         already_placed.append((i, j))
         sensor_map.append(item)
     
+    # Return list of sensors
     return sensor_map
 
-
+# Define terrain 
 terrain_width = 100
 terrain_height = 100
 terrain = Configuration(terrain_width, terrain_height)
 my_path = Path(__file__).parent / "../data/terrain/terrain2.csv"
 terrain.load_from_csv(my_path)
 
-
+# Initialize optimal sensor configuration cost and map
 best_score = 0
 best_config = None
 
 config_history = []
 
+# TASK double check what try is for
 try:
+    # Define number of sensors and radii
     num_sensors = 4
     if CASE == 1 or CASE == 2:
         sensor_radius = 20
@@ -83,16 +93,15 @@ try:
         sensor_radius = np.random.uniform(low = 10, high = 20, size = num_sensors)
     for i in range(MAX_ITERS):
         print("Iteration: ", i, end="\r")
-
         
+        # Assign sensors to grid points
         _map = make_basic_seismic_map(num_sensors, sensor_radius, terrain_width, terrain_height, terrain)
 
-
-
-
+        # Calculate cost and check if configuration is valid
         score = terrain.get_configaration_observability(_map)
         valid = terrain.is_configuration_valid(_map)
 
+        # Record each valid configuration that has a cost greater than the previous max
         if score > best_score and valid:
             print("New best score: ", score)
             best_score = score
@@ -100,6 +109,8 @@ try:
             config_history.append(_map)
     # print(best_score)
     # pp.pprint(best_config)
+            
+    # Save each new best configuration
     dirpath = './case_' + str(CASE)
     if os.path.exists(dirpath) and os.path.isdir(dirpath):
         shutil.rmtree(dirpath)
@@ -109,6 +120,7 @@ try:
     terrain.save_history(config_history,dirpath)
     terrain.plot_grid(best_config)
 
+# Save current best configuration if user stops before max iteration
 except KeyboardInterrupt:
     print("Stopped early")
     print(best_score)
