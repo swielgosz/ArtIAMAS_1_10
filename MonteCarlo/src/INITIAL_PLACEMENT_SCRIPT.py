c1c97193e78e9f3f766f689be158192fcc477d0a
JMocklerUMD <jmockle1@umd.edu>
2024-07-03 16:06:24 -0400

    we get some neat early results! Run the script and see!
    I think when we have more complicated meshing geometries, this will be interesting

--- MonteCarlo/src/INITIAL_PLACEMENT_SCRIPT.py
+++ MonteCarlo/src/INITIAL_PLACEMENT_SCRIPT.py
@@ -31,34 +31,12 @@ terrain.load_from_csv(my_path)
 
 # INITIAL SENSOR LIST
 # Characteristic list
-sensor_rad = [25, 25, 25]
-sensor_type = ["seismic","acoustic","seismic"]
+sensor_rad = [100, 100, 100, 100]
+sensor_type = ["seismic","acoustic","seismic", "acoustic"]
 num_sensors = len(sensor_type)
 sensor_comm_ratio = 1.5 # ratio of sensor communication to sensing radius 
-meas_type = ["radius", "bearing", "radius"] #radius or bearing
-LOS_flag = 1 # 1 if want to consider LOS, 0 if don't want to
-
-# Individal sensors
-targets = []
-sensor_locs = []
-tar1 = [37, 39]
-tar2 = [42, 32] 
-tar3 = [47, 39] 
-tar4 = [41, 45] 
-targets.extend(tar1)
-targets.extend(tar2)
-targets.extend(tar3)
-targets.extend(tar4)
-
-sens1 = [29, 44] 
-sens2 = [33, 29] 
-sens3 = [58, 34] 
-#sens4 = [44, 29] 
-sensor_locs.extend(sens1)
-sensor_locs.extend(sens2)
-sensor_locs.extend(sens3)
-#sensor_locs.extend(sens4)
-
+meas_type = ["radius", "bearing", "radius", "bearing"] #radius or bearing
+LOS_flag = 0 # 1 if want to consider LOS, 0 if don't want to
 
 # OPTIMIZATION PARAMETERS
 threshold = 8 #minimum distance a sensor must be from a target
@@ -67,7 +45,6 @@ threshold = 8 #minimum distance a sensor must be from a target
 d_sens_min = 4 #minimum sensor-sensor distance
 vol_tol = 1e-18
 maxfun_1 = 50000 #Max fun w/o LOS
-maxfun_2 = 50000 #Max fun w/ LOS
 max_iters = 20000
 printer_counts = 5000 #print the results every ___ fcn calls
 # Use list to run a parameter sweep
@@ -75,25 +52,7 @@ vol_tol = [1e-30]
 # vol_tol = [1e-14] #optimization param
 # ------------------------------------------
 
-## DETERMINE THE LOCALIZABLE TARGETS
-inputs = sensor_rad, meas_type, sensor_locs, targets, None
-(target_localized_successfully, _) = sensor_localization_routine(inputs)
-target_localized_successfully = [1,1,1,1]
-
-## NOMINAL FIM MAP SCORE
-inputs = target_localized_successfully, targets, sensor_locs, sensor_rad,meas_type, terrain, LOS_flag
-(FIMs, det_sum) = build_map_FIMS(inputs)
-print(FIMs)
-print("Nominal total FIM score")
-# print(FIMs)
-print(det_sum)
-
-# ------------------------------------------
-## ADDITIONAL PLACEMENT:
-# Using a list of available sensors, this section will 
-# Numerically solve for the optimal placement of the sensor
-# by maximizing the FIM determinant
-
+# ----------INITIAL PLACEMENT---------------
 # Now define the objective fcn
 def objective_fcn(x, *args):
     global counter
@@ -102,31 +61,13 @@ def objective_fcn(x, *args):
 
     counter += 1
     # (0) PRELIMINARIES
-    # Initialize the sensor lists
-    sensor_rad, sensor_type, meas_type, sensor_positions, num_sensors = [], [], [], [], 0
-
     # Pull out the old and new sensor lists
-    existing_sensor_lists, new_sensor_lists, target_inputs = args
+    sensor_chars, target_inputs = args
 
-    sensor_positions_old, sensor_rad_old, sensor_type_old, num_sensors_old, sensor_comm_ratio, meas_type_old = existing_sensor_lists
-    sensor_rad_new, sensor_type_new, num_sensors_new, sensor_comm_ratio_new, meas_type_new = new_sensor_lists
-    targets, target_localized_successfully, terrain = target_inputs
-
-    # Augment the lists with the placed and additionally placed sensors
-    sensor_rad.extend(sensor_rad_old)
-    sensor_rad.extend(sensor_rad_new)
+    sensor_rad, sensor_type, num_sensors, sensor_comm_ratio, meas_type = sensor_chars
+    target_mesh, terrain = target_inputs
     
-    sensor_type.extend(sensor_type_old)
-    sensor_type.extend(sensor_type_new)
-    
-    meas_type.extend(meas_type_old)
-    meas_type.extend(meas_type_new)
-    
-    sensor_positions.extend(sensor_positions_old)
-    
-    num_sensors += num_sensors_old 
-    num_sensors += num_sensors_new
-
+    sensor_positions = []
     # Add the optimization variables to the lists
     for k in range(len(x)//2):
         i, j = x[0+2*k], x[1+2*k] #let these vary and not be ints
@@ -136,16 +77,25 @@ def objective_fcn(x, *args):
     _map = make_basic_seismic_map(num_sensors, sensor_rad, sensor_type, meas_type, sensor_comm_ratio, sensor_positions)
 
     # (1) CHECK FOR VALID CONFIGURATIONS
+    # Relic of the past that I'm too lazy to change - just leave as true
     valid_placement_check, valid_WSN = True, True
 
-    inputs = target_localized_successfully, targets, sensor_positions, sensor_rad, meas_type, terrain, LOS_flag
+    target_localized_successfully = [1 for _ in range(len(target_mesh))] #just need a list of 1's
+    
+    # Break out the mesh pts:
+    targets_in = []
+    for target in target_mesh:
+        for pt in target:
+            targets_in.append(pt) 
+
+    inputs = target_localized_successfully, targets_in, sensor_positions, sensor_rad, meas_type, terrain, LOS_flag
     (FIMs, det_sum) = build_map_FIMS(inputs)
     det_mult = 1.
     tr_sum = 0.
 
     # Set optimizer_var to whatever you want (trace, det, eigenvalue)
     # NOTE: penalties are applied to individual FIMS - see the build_map_FIMS for details!!!!!
-    optimizer_var = det_mult
+    optimizer_var = tr_sum
     for kk in range(len(FIMs)):
         # FIM correspond to target list one-to-one
         optimizer_var = optimizer_var*np.linalg.det(FIMs[kk])
@@ -170,7 +120,7 @@ def objective_fcn(x, *args):
     else:
         # Else, calculate the penalty on WSN
         # Note: only ONE comm radii is accepted. Not written to vary (just yet!)
-        comm_radii = sensor_rad_new[0]*sensor_comm_ratio # just some scalar
+        comm_radii = sensor_rad[0]*sensor_comm_ratio # just some scalar
         valid_WSN_penalty = WSN_penalty(sensor_positions, comm_radii) 
 
     optimizer_var = optimizer_var*valid_WSN_penalty
@@ -193,31 +143,23 @@ def objective_fcn(x, *args):
             print(counter, 0, sensor_positions, valid_WSN, valid_placement_check)
         return 0
 
-# ------------------------------------------
-# Set a list of additional sensors to place down
-# SENSOR LIST
-sensor_rad_new = [100, 100, 100]
-sensor_type_new = ["seismic", "seismic", "acoustic"]
-num_sensors_new = len(sensor_type_new)
-sensor_comm_ratio_new = sensor_comm_ratio # ratio of sensor communication to sensing radius 
-meas_type_new = ["radial", "radial","bearing"]
+# ---------RUN THE OPTIMIZER-------------------
 
-# Now we run the optimizer w/ and w/o LOS considerations
-final_pos_LOS = []
-final_pos_no_LOS = []
+# Generate target mesh here!
+target_mesh_pts = [[50, 50],[49, 49],[51, 51], [60, 60], [62, 62], [58, 58], [40, 40], [42, 42]]
 
 for i in range(1):
     maxfun = maxfun_1
 
     # Construct tuples to pass in
     LOS_flag = 0
-    existing_sensor_lists = (sensor_locs, sensor_rad, sensor_type, num_sensors, sensor_comm_ratio, meas_type)
-    new_sensor_lists = (sensor_rad_new, sensor_type_new, num_sensors_new, sensor_comm_ratio_new, meas_type_new)
-    target_ins = (targets, target_localized_successfully, terrain)
+    
+    sensor_chars = (sensor_rad, sensor_type, num_sensors, sensor_comm_ratio, meas_type)
+    target_ins = (target_mesh_pts, terrain)
 
     # Set the bounds (controls the number of variables to reason over)
-    bounds = [(0, terrain_width-1),(0, terrain_height-1)]*(num_sensors_new) #bounds placing points to only on the map
-    sensor_list = (existing_sensor_lists, new_sensor_lists, target_ins)   
+    bounds = [(0, terrain_width-1),(0, terrain_height-1)]*(num_sensors) #bounds placing points to only on the map
+    sensor_list = (sensor_chars, target_ins)   
 
     # Call the optimizer!
     counter = 0
@@ -226,7 +168,7 @@ for i in range(1):
     # so we can check progress
     fig = plt.figure()
     ax_opt = fig.add_subplot()
-    for count, vol_tol_test in enumerate(vol_tol):
+    for count, vol_tol_test in enumerate(vol_tol): #lets you run multiple tolerances if you want
         fcn_eval_list, fcn_counter = [], []
         print("Start optimizer")
         res = optimize.direct(objective_fcn, bounds=bounds, args = sensor_list, vol_tol=vol_tol_test, maxfun = maxfun, maxiter = max_iters)
@@ -234,44 +176,31 @@ for i in range(1):
         print(res.message, "Final values:", res.x)
         x_out = res.x
 
-    if i == 0:
-        final_pos_no_LOS = sensor_locs.copy()
-        final_pos_no_LOS.extend(x_out)
-        ax_opt.set_title('Function Evaluation across Optimization - No LOS considerations')
-        print(min_sensor_distance_penalty(final_pos_no_LOS, d_sens_min))
-    if i == 1:
-        final_pos_LOS = sensor_locs.copy()
-        final_pos_LOS.extend(x_out)
-        ax_opt.set_title('Function Evaluation across Optimization - LOS considerations')
+    # Plot formatting
+    ax_opt.set_title('Initial Placement Optimization')
+    ax_opt.set_ylabel('Objective Function Evaluations'), ax_opt.set_xlabel('Iteration Count')
+    #ax_opt.set_ylim([min(fcn_eval_list)*1.25, max(fcn_eval_list)*1.25])
+    ax_opt.grid(True, which='minor')  
+    plt.show()
+    
+    # Final optimized values!
+    final_sensor_locs = x_out
 
 print('-----------------------------------')
-print("w/o LOS positions:", final_pos_no_LOS)
-print("w/ LOS positions :", final_pos_LOS)
 
-# ------------------------------------------
+# ---------FORMAT AND ANALYSIS------------------
 # Formatting
-ax_opt.set_ylabel('Objective Function Evaluations'), ax_opt.set_xlabel('Iteration Count')
-#ax_opt.set_ylim([min(fcn_eval_list)*1.25, max(fcn_eval_list)*1.25])
-ax_opt.grid(True, which='minor')  
-plt.show()
-
-# Create the finalized lists and create a new map
-num_sens_total = num_sensors+num_sensors_new
-sensor_rad_total, sensor_type_total, meas_type_total = [], [], []
-for i in range(num_sensors):
-    sensor_rad_total.append(sensor_rad[i])
-    sensor_type_total.append(sensor_type[i])
-    meas_type_total.append(meas_type[i])
-
-for i in range(num_sensors_new):
-    sensor_rad_total.append(sensor_rad_new[i])
-    sensor_type_total.append(sensor_type_new[i])
-    meas_type_total.append(meas_type_new[i])
 
 
 # Finally, make the map for plotting
 # Localize the new map
-inputs = target_localized_successfully, targets, final_pos_no_LOS, sensor_rad_total, meas_type_total, terrain, 0 #LOS_flag == 1
+target_localized_successfully = [1 for _ in range(len(target_mesh_pts ))] #just need a list of 1's 
+targets_in = []
+for target in target_mesh_pts:
+    for pt in target:
+        targets_in.append(pt) 
+
+inputs = target_localized_successfully, targets_in, final_sensor_locs, sensor_rad, meas_type, terrain, 0 #LOS_flag == 1
 (FIMs_no_LOS, det_sum) = build_map_FIMS(inputs)
 print("No LOS Considerations in planning:", det_sum)
 print("No LOS Considerations in planning:", FIMs_no_LOS)
@@ -282,19 +211,16 @@ print("No LOS Considerations in planning:", FIMs_no_LOS)
 #print("LOS Considerations in planning:", FIMs_LOS)
 
 # Finally, construct the plot and add the ellipses
-_map = make_basic_seismic_map(num_sens_total, sensor_rad_total, sensor_type_total, meas_type_total, sensor_comm_ratio, final_pos_no_LOS)
+_map = make_basic_seismic_map(num_sensors, sensor_rad, sensor_type, meas_type, sensor_comm_ratio, final_sensor_locs)
 ax = terrain.plot_grid(_map)
-new_map = add_targets(ax, targets)
-for i in range(len(targets)//2):
-    target_i = [targets[0+2*i], targets[1+2*i]]
+new_map = add_targets(ax, targets_in)
+for i in range(len(targets_in)//2):
+    target_i = [targets_in[0+2*i], targets_in[1+2*i]]
     new_map = plot_uncertainty_ellipse(new_map, FIMs_no_LOS[i], target_i, 2.48, 1, "grey", "analytical")
     #new_map = plot_uncertainty_ellipse(new_map, FIMs_LOS[i], target_i, 2.48, 1, "black", "analytical")
 
 # Add some text to the plot to show initial vs final placed sensors
-for i in range(len(sensor_locs)//2):
-    plt.text(sensor_locs[0+2*i]+1, sensor_locs[1+2*i]+1, "In")
-
-for i in range(len(x_out)//2):
-    plt.text(x_out[0+2*i]+1, x_out[1+2*i]+1, "F") #plots the LOS sensors
+for i in range(len(final_sensor_locs)//2):
+    plt.text(final_sensor_locs[0+2*i]+1, final_sensor_locs[1+2*i]+1, "In")
 
 plt.show()
